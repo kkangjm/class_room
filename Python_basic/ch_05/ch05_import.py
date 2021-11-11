a17279c4fabff128e83d2b8470308f14b20d057b
JM <79086289+kkangjm@users.noreply.github.com>
2021-11-11 16:10:08 +0900

    Add files via upload

---
+++ Python_basic/ch_05/ch05_import.py
@@ -0,0 +1,8 @@
+
+import ch05_function as f
+
+print('imported calc_test', f.calc_test(3, 4))
+print('imported calc_test', f.calc_test(3, 4, '*'))
+print('imported return_test', f.return_test(5, 4))
+
+print(dir('__builtin__'))
