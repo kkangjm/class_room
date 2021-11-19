84efcab01b59881fdbd306417a1a56950d2f3785
JM <79086289+kkangjm@users.noreply.github.com>
2021-11-19 15:01:03 +0900

    Update ch05_function.py

--- Python_basic/ch_05/ch05_function.py
+++ Python_basic/ch_05/ch05_function.py
@@ -9,7 +9,7 @@ def calc_test (a, b, operator='+'):
     else : print('ERROR')
     return result
 
-# print('결과 = ', calc_test(2, 10, '**'))
+print('결과 = ', calc_test(2, 10, '**'))
 
 
 def return_test(a, b):
@@ -17,13 +17,12 @@ def return_test(a, b):
     minus = a - b
     return plus, minus
 
-# print(calc_test(3, 4, '*'))
-# print(return_test(5, 4))
+print(calc_test(3, 4, '*'))
+print(return_test(5, 4))
 
 
 
-if __name__ == '__main__':
-
-    print(calc_test(3, 4))
-    print(calc_test(3, 4, '*'))
-    print(return_test(5, 4))
+# if __name__ == '__main__':
+#     print(calc_test(3, 4))
+#     print(calc_test(3, 4, '*'))
+#     print(return_test(5, 4))
