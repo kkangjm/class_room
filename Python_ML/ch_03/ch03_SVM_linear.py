78bc9529f62b1e0a3d155bcce997b14d6e647ee4
JM <79086289+kkangjm@users.noreply.github.com>
2021-12-10 20:21:15 +0900

    Update ch03_SVM_linear.py

--- Python_ML/ch_03/ch03_SVM_linear.py
+++ Python_ML/ch_03/ch03_SVM_linear.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 import matplotlib.pyplot as plt
