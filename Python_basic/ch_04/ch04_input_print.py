e8d6fceb32bb31048c08d2a3884193339de2161f
JM <79086289+kkangjm@users.noreply.github.com>
2023-06-02 15:07:13 +0900

    Delete Python_basic directory

--- Python_basic/ch_04/ch04_input_print.py
+++
@@ -1,9 +0,0 @@
-
-money = input ('금액 = ')
-print(type(money))
-print('현재 잔액은', money, '원 입니다.')
-print('잔액은 \n', money)
-print('총 자산 =', int(money) + 10000)
-print('현금', money, sep=':')
-print('보유액 =', end=' ')
-print(money)
--- Python_basic/ch_05/ch05_import.py
+++
@@ -1,7 +0,0 @@
-
-import ch05_function as f
-
-print('imported calc_test', f.calc_test(3, 4))
-print('imported calc_test', f.calc_test(3, 4, '*'))
-print('imported return_test', f.return_test(5, 4))
-
